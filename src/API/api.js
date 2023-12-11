f1fde89f15894fe17809937db9171acba42e0a75
ttemuchin <mokoseev.artem@gmail.com>
2023-12-11 13:53:35 +0300

    api

---
+++ src/API/api.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+const instance = axios.create({
+    baseURL: "http://127.0.0.1:8000/",
+    withCredentials: true,
+    headers: {"Content-Type": "application/json"}
+})
+
+export const validationAPI = {
+    loginApi(email, password) {
+        return instance.post("/login", {email, password})
+    },
+    registrationApi(email, password) {
+        return instance.post("/registration", {email, password})
+    }
+}
+
+export const bookingAPI = {
+    bookingApi(email, name, date) {
+        return instance.post("/booking", {email, name, date})
+    },
+    bookingApi(email, name, date) {
+        return instance.put("/booking", {email, name, date})
+    }
+}
